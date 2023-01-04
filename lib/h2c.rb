f889ebe8b701f89ed689402f808a1b5320058339
azuchi <azuchi@chaintope.com>
2023-01-04 17:05:43 +0900

    Remove ffi

--- lib/h2c.rb
+++ lib/h2c.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "h2c/version"
-require "ffi"
 
 # Hash to Curves library.
 # https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-16.html
 module H2C
-  autoload :FFI, "h2c/ffi"
 
   class Error < StandardError
   end
