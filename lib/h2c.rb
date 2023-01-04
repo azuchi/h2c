47a2587dda08703e094c6feafb43858c57ffd20f
azuchi <azuchi@chaintope.com>
2023-01-04 17:02:59 +0900

    Add prettier and rubocop

--- lib/h2c.rb
+++ lib/h2c.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "h2c/version"
+require "ffi"
 
+# Hash to Curves library.
+# https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-16.html
 module H2C
-  class Error < StandardError; end
+  autoload :FFI, "h2c/ffi"
+
+  class Error < StandardError
+  end
   # Your code goes here...
 end
--- spec/h2c_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe H2C do
-  it "has a version number" do
-    expect(H2C::VERSION).not_to be nil
-  end
-end
